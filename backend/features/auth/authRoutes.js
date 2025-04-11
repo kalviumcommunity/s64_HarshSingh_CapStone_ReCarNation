dc716800c692f4909e37d335872ca5094eb2fb1a
Harsh Singh <sky_walker@Harshs-MacBook-Air.local>
2025-04-11 17:12:01 +0530

    using jwt token to implementautherization

--- backend/features/auth/authRoutes.js
+++ backend/features/auth/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('./authController');
+const { isAuthenticated } = require('./middleware/authMiddleware');
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/get', authController.getAllEmails);
-
-// router.get('/profile', authController.authController.profile);
+router.get('/profile', isAuthenticated, authController.authController.profile);
 
 module.exports = router;
--- backend/features/products/productsRoutes.js
+++ backend/features/products/productsRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose')
 const productController = require('./productController');
+const { isAuthenticated } = require('../auth/middleware/authMiddleware');
 
 router.get('/', productController.getAllProducts);
-router.post('/', productController.createProduct);
-router.put('/:id', productController.updateProduct);
+router.post('/', isAuthenticated, productController.createProduct);
+router.put('/:id', isAuthenticated, productController.updateProduct);
 
 module.exports = router;
