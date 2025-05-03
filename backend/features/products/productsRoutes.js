93ae88877c8134507d93181d7c9624573afae74c
Harsh Singh <sky_walker@Harshs-MacBook-Air.local>
2025-05-03 10:29:12 +0530

    put api

--- backend/features/products/productsRoutes.js
+++ backend/features/products/productsRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose')
+const upload = require('../middleware/uploadMiddleware');
 const productController = require('./productController');
+const { isAuthenticated } = require('../auth/authMiddleware/authMiddleware');
 
-router.post('/', productController.createProduct);
-router.get('/', productController.getAllProduct);
+
+router.get('/', productController.getAllProducts);
+router.post('/', isAuthenticated, upload.array('images', 10), productController.createProduct);
 
 module.exports = router;
